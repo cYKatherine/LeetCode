3fbf7f61d6f1b26fc3d1b2738b3600b92df75b08
Katherine Chen <cykatherinee@gmail.com>
2022-08-06 23:07:51 +1000

    Add recursive solution for https://leetcode.com/problems/binary-tree-inorder-traversal/

---
+++ 94 - Recursive.cpp
@@ -0,0 +1,30 @@
+// Recursive solution for https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        traversal(root, res);
+        return res;
+    }
+    
+    void traversal(TreeNode* cur, vector<int>& res) {
+        if (cur) {
+            traversal(cur->left, res);
+            res.push_back(cur->val);
+            traversal(cur->right, res);
+        }
+        return;
+    }
+};
